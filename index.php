8492ef6c0d3e113f283ab1f1c4b9e923154fb1ae
Ryan McCahan <ryan@mccahan.net>
2015-04-23 12:13:35 -0600

    New index page

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$hideROISidebar = true;
+$hideDownloadLinks = true;
+$activePage = false;
+include('header.php'); ?>
+<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis lorem vitae tortor congue suscipit at quis dolor. Sed vehicula elit eu luctus rhoncus.</h2>
+<p>Nam consectetur arcu eget ipsum venenatis, at facilisis ligula facilisis. Fusce elit mauris, ornare sed tortor ut, suscipit maximus nulla. Morbi tincidunt ante id lacus luctus, at venenatis libero iaculis. Mauris neque est, aliquam quis tempus a, facilisis id eros. Curabitur nec blandit metus. </p>
+<p>Suspendisse iaculis metus sit amet augue dignissim porta. Cras blandit metus in sem faucibus, placerat suscipit leo ultricies. </p>
+<p>&nbsp;</p>
+<p><a class="btn btn-primary btn-lg btn-block" href="cost-hire.php">Get Started</a></p>
+<?php include('footer.php'); ?>
