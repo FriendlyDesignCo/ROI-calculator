725df16ca646a65b95790055710116dc6fa9d115
Ryan McCahan <ryan@mccahan.net>
2015-04-23 01:02:33 -0600

    Desktop layout demo

---
+++ roi-sidebar.php
@@ -0,0 +1,6 @@
+<div class="well text-center">
+  <div id="progress-holder"><div id="progress" style="width:23.75%;"></div></div>
+  <p>Estimated ROI</p>
+  <p id="estimated-roi">4129%</p>
+  <a href="#" id="expand-roi"><i class="glyphicon glyphicon-chevron-down"></i></a>
+</div>
