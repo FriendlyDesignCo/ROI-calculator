3ec5be7c3a656d420a516a5e7cd6b9edd5483d5d
Ryan McCahan <ryan@mccahan.net>
2015-06-19 13:57:07 -0600

    ROI sidebar show visual percentage indicator
    Fixes #1

--- roi-sidebar.php
+++ roi-sidebar.php
@@ -1,5 +1,10 @@
+<?php $progressPercent = 0;
+foreach ($_SESSION['progress'] as $key => $value)
+  if ($value === true)
+    $progressPercent += 25;
+?>
 <div class="well text-center">
-  <div id="progress-holder"><div id="progress" style="width:23.75%;"></div></div>
+  <div id="progress-holder"><div id="progress" style="width:<?php echo $progressPercent; ?>%;"></div></div>
   <p>Estimated ROI</p>
   <p id="estimated-roi">4129%</p>
   <a href="#" id="expand-roi"><i class="glyphicon glyphicon-chevron-down"></i></a>
