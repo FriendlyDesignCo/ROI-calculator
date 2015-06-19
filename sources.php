d9b6c469d81b97d47e6796c95c8044f667417ef8
Ryan McCahan <ryan@mccahan.net>
2015-06-19 13:28:00 -0600

    Reformat Sources page
    Fixes #13

--- sources.php
+++ sources.php
@@ -7,14 +7,14 @@ include('header.php'); ?>
 <p>The following sources were used in the ROI calculator:</p>
 
 <h2>Advertising Cost</h2>
-<ul>
-  <li><a href="http://www.brookings.edu/research/interactives/2014/job-vacancies-and-stem-skills#/M10420">Brookings Report - STEM Vacancies</a></li>
-  <li>According to one survey, the average U.S. company spends $3,500 per new hire in job advertising costs and human resources staff, or $124 billion per year.<br><em>That figure includes at least some part of human resource staff salaries and comes from Karen O’Leonard, “The Talent Acquisition Factbook 2011” (Oakland, CA: Bersin & Associates, 2011). Estimates from the United Kingdom are comparable at about $2,700 per hire for entry-level college graduates; CFE Research, “The AGR Graduate Recruitment Survey 2013” (Leicester, U.K., 2013).</em></li>
-</ul>
+
+<p><a href="http://www.brookings.edu/research/interactives/2014/job-vacancies-and-stem-skills#/M10420">Brookings Report - STEM Vacancies</a><br>
+According to one survey, the average U.S. company spends $3,500 per new hire in job advertising costs and human resources staff, or $124 billion per year.</p>
+<p>That figure includes at least some part of human resource staff salaries and comes from Karen O’Leonard, “The Talent Acquisition Factbook 2011” (Oakland, CA: Bersin & Associates, 2011). Estimates from the United Kingdom are comparable at about $2,700 per hire for entry-level college graduates; CFE Research, “The AGR Graduate Recruitment Survey 2013” (Leicester, U.K., 2013).</em></p>
+
+<hr>
 
 <h2>Business Impact</h2>
-<ul>
-  <li><a href="http://www.themanufacturinginstitute.org/Research/Skills-and-Training-Study/Skills-and-Training-Study.aspx">2014 Manufacturing Skills &amp; Training Study</a></li>
-</ul>
+<p><a href="http://www.themanufacturinginstitute.org/Research/Skills-and-Training-Study/Skills-and-Training-Study.aspx">2014 Manufacturing Skills &amp; Training Study</a></p>
 
 <?php include('footer.php'); ?>
