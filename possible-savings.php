9619f453abb39fb66ab07a56b1b963394b2ffac0
Ryan McCahan <ryan@mccahan.net>
2015-06-25 14:02:19 -0600

    Dotted lines in Possible Savings
    Fixes #37

--- possible-savings.php
+++ possible-savings.php
@@ -10,6 +10,7 @@ include('header.php'); ?>
 <h2>Reduction in Turnover</h2>
 <p>Consider an additional perk companies frequently realize from hiring from a better candidate pool: These employees are a better fit for the job, and they typically stay longer in the position. This benefit is actually realized the following year because less employees need to be hired and trained. Companies report that employee retention may <strong>improve by 50% or more</strong> when they hire candidates with an industry certification that matches their needs.</p>
 <p><strong>DIRECTIONS:</strong> Enter a percent reduction you expect to achieve in turnover for your new, better-qualified employees.</p>
+<hr class="dotted">
 
 <div class="row">
   <div class="col-md-6">
@@ -60,6 +61,8 @@ include('header.php'); ?>
 <p>By partnering with a community college, you can lay out your minimum and optimal competencies and help establish a recruiting relationship. One of the most important steps you can take is to identify manufacturing certifications that validate the skills needed on the job.</p>
 <p>How much difference can this make? Businesses that have partnered with a community college have estimated it can <strong>reduce by 50% or more</strong> the number of candidates they have to interview.</p>
 <p><strong>DIRECTIONS:</strong> Enter a percent reduction you expect to achieve from a partnership that helps you build a pipeline of workers with the right skills. (If you’re not sure, you may leave the figure at 50% to get an idea of the possible cost savings.)</p>
+
+<hr class="dotted">
 
 <div class="row">
   <div class="col-md-6">
@@ -139,6 +142,8 @@ include('header.php'); ?>
 <p>If you rely on temporary/placement agencies for some or all of your technical hires, that’s another potential area where you can save. Some businesses have completely eliminated temporary agencies, and the fees they pay, when they build a robust community college partnership.</p>
 <p><strong>DIRECTIONS:</strong> Enter a percent reduction you expect to achieve in your staffing/placement fees. (If you didn’t enter any staffing agency fees before, you can skip this step.)</p>
 
+<hr class="dotted">
+
 <div class="row">
   <div class="col-md-8">
     <label>Reduction/Elimination of Staffing Agency Fees</label>
@@ -166,6 +171,8 @@ include('header.php'); ?>
 <h2>Reduction in OJT Cost Per Employee</h2>
 <p>Frequently, better qualified candidates require less on-the-job training to come up to speed once hired. Many companies have reported a 33% reduction in OJT for employees that already possess an industry certification.</p>
 <p><strong>DIRECTIONS:</strong> Enter a percent reduction you expect to achieve in the training time required for a new employee.</p>
+
+<hr class="dotted">
 
 <div class="row">
   <div class="col-md-8">
@@ -213,6 +220,8 @@ include('header.php'); ?>
 <h2>Reduction in Overtime Cost</h2>
 <p>As you saw in the previous section on Business Impact, overtime needed to cover unfilled positions represents a significant expense. Having a pipeline of qualified candidates is a source of savings that can positively benefit your bottom line.</p>
 
+<hr class="dotted">
+
 <div class="row">
   <div class="col-md-6">
     <label>Savings due to reduction in overtime</label>
@@ -221,6 +230,7 @@ include('header.php'); ?>
     <p class="subtotal" id="reduction-overtime-impact">$</p>
   </div>
 </div>
+<hr>
 <br><br><br>
 
 <p class="text-center total-description">Total Possible Savings</p>
