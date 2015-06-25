b7145f9ab0260b63dfc0d00e16eb82e55518f375
Ryan McCahan <ryan@mccahan.net>
2015-06-25 12:06:36 -0600

    Hide sidebar ROI on smaller sizes

--- roi-sidebar.php
+++ roi-sidebar.php
@@ -3,7 +3,7 @@ foreach ($_SESSION['progress'] as $key => $value)
   if ($value === true)
     $progressPercent += 25;
 ?>
-<div class="well text-center">
+<div class="well text-center hidden-sm hidden-xs">
   <div id="progress-holder"><div id="progress" style="width:<?php echo $progressPercent; ?>%;"></div></div>
   <p>Estimated ROI</p>
   <p id="estimated-roi">4129%</p>
