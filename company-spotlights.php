d543750a7a38bf51a56bbffa3ae6eb6429031053
Ryan McCahan <ryan@mccahan.net>
2016-07-29 02:46:18 -0600

    Spotlights and How-To Guide

---
+++ company-spotlights.php
@@ -0,0 +1,24 @@
+<?php
+$hideROISidebar = true;
+$hideDownloadLinks = true;
+$activePage = false;
+include('header.php'); ?>
+<h2>Company Spotlights</h2>
+<p>Pour-over vice swag, tilde humblebrag umami health goth photo booth stumptown forage single-origin coffee seitan direct trade. Swag pitchfork venmo, kale chips gastropub flannel yuccie kogi. Synth occupy actually before they sold out, man bun portland flannel vice try-hard. Post-ironic food truck yr, scenester gluten-free photo booth bushwick. Semiotics yr cred listicle fanny pack, banjo man bun. Bespoke blue bottle keffiyeh hoodie skateboard, beard selvage franzen marfa iPhone green juice polaroid drinking vinegar ugh. Gentrify cray swag, chillwave pabst polaroid portland hella seitan chambray paleo scenester sriracha.</p>
+<p>&nbsp;</p>
+<div class="spotlights">
+  <a href="case-studies/Magnet%20ROI.pdf" class="spotlight">
+    <div class="row">
+      <div class="col-sm-3"><img src="case-studies/CaseStudy-NortheastOhio.png" alt="Manufacturing Institute’s Return on Investment Calculator Case Study: Northeast Ohio" width="400" height="478"></div>
+      <div class="col-sm-9"><p class="link">Case Study: Northeast Ohio</p></div>
+    </div>
+  </a>
+  <a href="case-studies/Stober%20Drives%20ROI.pdf" class="spotlight">
+    <div class="row">
+      <div class="col-sm-3"><img src="case-studies/CaseStudy-StoberDrives.png" alt="Manufacturing Institute’s Return on Investment Calculator Case Study: Stober Drives" width="400" height="478"></div>
+      <div class="col-sm-9"><p class="link">Case Study: Stober Drives</p></div>
+    </div>
+  </a>
+</div>
+<p><a class="btn btn-primary btn-lg btn-block" href="cost-hire.php">Begin Your Calculations</a></p>
+<?php include('footer.php'); ?>
--- index.php
+++ index.php
@@ -8,4 +8,8 @@ include('header.php'); ?>
 <p>Complete this easy-to-use Return on Investment Calculator to add up the costs to your operation. Plus see how some proven HR solutions can improve your efficiencies, reduce recruiting expenses, and likely pay for themselves.</p>
 <p>&nbsp;</p>
 <p><a class="btn btn-primary btn-lg btn-block" href="cost-hire.php">Get Started</a></p>
+<div class="button-box row">
+  <div class="col-md-6"><a href="company-spotlights.php" class="btn btn-secondary btn-lg btn-block"><i class="glyphicon glyphicon-star"></i> Company Spotlights</a></div>
+  <div class="col-md-6"><a href="MI_CostofaHire_HowToGuide.pdf" target="_blank" class="btn btn-secondary btn-lg btn-block"><i class="glyphicon glyphicon-th-list"></i> How-To Guide</a></div>
+</div>
 <?php include('footer.php'); ?>
